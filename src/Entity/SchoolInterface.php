92393b1c818f6b720494c802d2768337a5c9adbf
NikLP <NikLP@users.noreply.github.com>
2025-08-31 16:25:25 +0100

    Updated interface class

--- src/Entity/SchoolInterface.php
+++ src/Entity/SchoolInterface.php
@@ -3,10 +3,13 @@
 namespace Drupal\ascend_school\Entity;
 
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\user\EntityOwnerInterface;
 
 /**
  * Interface for School entities.
  */
-interface SchoolInterface extends ContentEntityInterface {
-
+interface SchoolInterface extends ContentEntityInterface, EntityChangedInterface, EntityOwnerInterface, EntityPublishedInterface {
+  
 }
